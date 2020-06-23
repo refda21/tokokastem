453564da8a933b16fe93fe68777b8ad39c53e4c4
refda <muhammadrefda@gmail.com>
2020-06-23 19:13:10 +0700

    adding html template to project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index');
 });
+
+Route::get('products', function (){
+        return view('products.detail');
+})->name('products.details');
+
+
+Route::get('products/custom', function (){
+    return view('products.custom');
+})->name('products.custom');
+
+
+Route::get('account', function (){
+    return view('account');
+})->name('account');
+
+Route::get('contact', function (){
+    return view('contact');
+})->name('contact');
+
+Route::get('cara-pesan', function (){
+    return view('cara-pesan');
+})->name('cara-pesan');
+
+Route::get('products/payment', function (){
+    return view('products.payment');
+})->name('products.payment');
+
