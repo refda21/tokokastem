f237a898f5d5eae92507e683aaa69fc95afcba3e
refda <muhammadrefda@gmail.com>
2020-07-09 00:20:15 +0700

    fixing the template for front end and admin panel v2

---
+++ database/migrations/2020_06_30_081247_create_stock_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
